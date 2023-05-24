adcf987ecf08cd8ffca8bc6836a82453da2572d2
theunknownIR <theunknownIR@gmail.com>
2023-05-24 12:15:52 -0700

    plz work

--- shell.c
+++ shell.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-
-#define D_PROMPT "$ "
+#include "shell.h"
 
 /**
  * execute_command - Tokenizes user input and executes the command
